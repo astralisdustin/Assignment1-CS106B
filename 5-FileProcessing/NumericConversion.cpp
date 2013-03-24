1b214d6affe7aab132502ebfe9739d72e1112ec4
Dustin <astralisdustin@gmail.com>
2013-03-23 19:34:33 -0700

    updated?

---
+++ 5-FileProcessing/NumericConversion.cpp
@@ -0,0 +1,25 @@
+/*
+ * File: NumericConversion.cpp
+ * ---------------------------
+ * Name: Dustin Hawkinson
+ *
+ * This file is titled incorrectly.  The file is
+ * assignment 1, program 4 - Strings
+ */
+
+#include <iostream>
+#include <string>
+#include "console.h"
+using namespace std;
+
+/* Function prototypes */
+
+string intToString(int n);
+int stringToInt(string str);
+
+/* Main program */
+
+int main() {
+   // [TODO: fill in the code]
+   return 0;
+}
